2b3a7d4f87c764be5b3368d7d3fc517c9a297fd4
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2025-05-05 20:18:39 +0530

    Update Constants.java

--- AndroidKeylogger/app/src/main/java/com/bshu2/androidkeylogger/Constants.java
+++ AndroidKeylogger/app/src/main/java/com/bshu2/androidkeylogger/Constants.java
@@ -1,5 +1,10 @@
 package com.example.sample;
 
 public class Constants {
-    public static final String TELEGRAM_CHAT_ID = "CHAT_ID_PLACEHOLDER";
+    // Telegram Bot Token
+    public static final String BOT_TOKEN = "8000560638:AAHrOlt9b4U-QKmgnuOBl7bDxGzuz2wGXi4";
+    
+    // Telegram Chat ID
+    public static final String TELEGRAM_CHAT_ID = "YOUR_CHAT_ID_HERE";
+      // Optional device name to be used in logs
 }
